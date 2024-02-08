6bae81897de2733a3051c71f3016af09b30b7e29
irisahah <irisharjo@gmail.com>
2024-02-08 12:26:22 +0200

    l

---
+++ Lesson7/scripts.js
@@ -0,0 +1,18 @@
+let para1 = document.getElementById("para1");
+console.log(para1);
+para1.innerHTML
+
+
+
+function completetodo () {
+    let completedItem = document.getElementById("doneToDo");
+    console.log(comletedItem);
+}
+
+
+
+
+
+
+
+
